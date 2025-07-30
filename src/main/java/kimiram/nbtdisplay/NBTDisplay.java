d7457655d1052770f3d5d5ef3dee70773f5658d9
Kimiram <kmrmkrs9@gmail.com>
2025-07-30 23:02:32 +0300

    1.21.6 update

--- src/main/java/kimiram/nbtdisplay/NBTDisplay.java
+++ src/main/java/kimiram/nbtdisplay/NBTDisplay.java
@@ -1,5 +1,6 @@
 package kimiram.nbtdisplay;
 
+import com.mojang.serialization.DataResult;
 import net.fabricmc.api.ClientModInitializer;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
@@ -12,6 +13,10 @@ import net.fabricmc.fabric.impl.client.screen.ScreenEventFactory;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.component.ComponentChanges;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -34,64 +39,68 @@ public class NBTDisplay implements ClientModInitializer {
 	public void onInitializeClient() {
 		ItemTooltipCallback.EVENT.register((stack, context, type, texts) -> {
             if (InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), KeyBindingHelper.getBoundKeyOf(showNbtKeybind).getCode())) {
-                String raw = stack.toNbt(Objects.requireNonNull(context.getRegistryLookup())).toString();
-                String line = "";
-                int spaces = 0;
+                DataResult<NbtElement> result = ComponentChanges.CODEC.encodeStart(Objects.requireNonNull(context.getRegistryLookup()).getOps(NbtOps.INSTANCE), stack.getComponentChanges());
+                NbtCompound compound = (NbtCompound) result.getOrThrow();
+                if (!compound.isEmpty()) {
+                    String raw = compound.toString();
+                    String line = "";
+                    int spaces = 0;
 
-                for (int i = 0; i < raw.length(); i++) {
-                    char chr = raw.charAt(i);
+                    for (int i = 0; i < raw.length(); i++) {
+                        char chr = raw.charAt(i);
 
-                    switch (chr) {
-                        case ':':
-                            line += ":";
-                            if (!Character.isLetter(raw.charAt(i + 1))) {
-                                line += " ";
-                            }
-                            break;
-                        case ',':
-                            line += ",";
-                            texts.add(Text.literal(line));
-                            line = " ".repeat(spaces);
-                            break;
-                        case '{':
-                            if (raw.charAt(i + 1) == '}') {
-                                line += "{}";
-                                i++;
+                        switch (chr) {
+                            case ':':
+                                line += ":";
+                                if (!Character.isLetter(raw.charAt(i + 1))) {
+                                    line += " ";
+                                }
                                 break;
-                            }
-
-                            line += "{";
-                            spaces += 2;
-                            texts.add(Text.literal(line));
-                            line = " ".repeat(spaces);
-                            break;
-                        case '[':
-                            if (raw.charAt(i + 1) == ']') {
-                                line += "[]";
-                                i++;
+                            case ',':
+                                line += ",";
+                                texts.add(Text.literal(line));
+                                line = " ".repeat(spaces);
                                 break;
-                            }
+                            case '{':
+                                if (raw.charAt(i + 1) == '}') {
+                                    line += "{}";
+                                    i++;
+                                    break;
+                                }
 
-                            line += "[";
-                            spaces += 2;
-                            texts.add(Text.literal(line));
-                            line = " ".repeat(spaces);
-                            break;
-                        case '}':
-                            texts.add(Text.literal(line));
-                            spaces -= 2;
-                            line = " ".repeat(spaces) + "}";
-                            break;
-                        case ']':
-                            texts.add(Text.literal(line));
-                            spaces -= 2;
-                            line = " ".repeat(spaces) + "]";
-                            break;
-                        default:
-                            line += chr;
+                                line += "{";
+                                spaces += 2;
+                                texts.add(Text.literal(line));
+                                line = " ".repeat(spaces);
+                                break;
+                            case '[':
+                                if (raw.charAt(i + 1) == ']') {
+                                    line += "[]";
+                                    i++;
+                                    break;
+                                }
+
+                                line += "[";
+                                spaces += 2;
+                                texts.add(Text.literal(line));
+                                line = " ".repeat(spaces);
+                                break;
+                            case '}':
+                                texts.add(Text.literal(line));
+                                spaces -= 2;
+                                line = " ".repeat(spaces) + "}";
+                                break;
+                            case ']':
+                                texts.add(Text.literal(line));
+                                spaces -= 2;
+                                line = " ".repeat(spaces) + "]";
+                                break;
+                            default:
+                                line += chr;
+                        }
                     }
+                    texts.add(Text.literal(line));
                 }
-                texts.add(Text.literal(line));
             }
 		});
 	}
